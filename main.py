828098972cf9a9c250d76db179e287fa1de963be
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-25 16:59:43 +1000

    update main.py
    - create asteroids group
    - add containers to Asteroid class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import Player
 
@@ -7,8 +8,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
